9fcb9284d0d65e8b51257c39fea0a3966f33e296
Robby6Strings <rausten93@gmail.com>
2023-11-04 08:10:34 +1300

    add browser-demo

---
+++ sandbox/browser-demo/src/counter.ts
@@ -0,0 +1,33 @@
+import useWorker from "async-worker-ts"
+
+const add = async (a: number, b: number) => a + b
+const getUser = async (userId: number) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  )
+  return response.json()
+}
+
+const worker = useWorker({ add, getUser })
+
+async function main() {
+  //await worker.commands.init()
+  await Promise.all([
+    worker.add(1, 2).then(console.log),
+    worker.getUser(1).then(console.log),
+  ])
+  await worker.deInit()
+}
+
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener("click", () => {
+    setCounter(counter + 1)
+    main()
+  })
+  setCounter(0)
+}
--- sandbox/demo/src/index.ts
+++ sandbox/demo/src/index.ts
@@ -2,7 +2,7 @@ import useWorker from "async-worker-ts"
 
 const worker = useWorker({
   add: async (a: number, b: number) => a + b,
-  getUser: async (userId: string) => {
+  getUser: async (userId: number) => {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/users/${userId}`
     )
@@ -12,7 +12,9 @@ const worker = useWorker({
 
 async function main() {
   const num = await worker.add(1, 2)
-  console.log(num)
+  const user = await worker.getUser(1)
+  console.log(num, user)
+  await worker.deInit()
 }
 
 main()
