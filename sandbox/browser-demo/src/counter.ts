547f84ae3ad06a5fea8528cc34370ce612159059
Robby6Strings <rausten93@gmail.com>
2023-11-05 00:46:59 +1300

    update sandbox/browser-demo to use task()

--- sandbox/browser-demo/src/counter.ts
+++ sandbox/browser-demo/src/counter.ts
@@ -1,20 +1,29 @@
-import useWorker from "async-worker-ts"
+import useWorker, { task } from "async-worker-ts"
 
-const add = async (a: number, b: number) => a + b
-const getUser = async (userId: number) => {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${userId}`
-  )
-  return response.json()
-}
+let userId = 1
 
-const worker = useWorker({ add, getUser })
+const worker = useWorker({
+  calculatePi: () => {
+    let pi = 0
+    for (let i = 0; i < 1_000_000; i++) {
+      pi += Math.pow(-1, i) / (2 * i + 1)
+    }
+    return pi * 4
+  },
+  loadUser: task(
+    async (id) => {
+      const user = await fetch(`https://dummyjson.com/users/${id}`)
+      return user.json()
+    },
+    () => [userId]
+  ),
+})
 
 async function main() {
   //await worker.commands.init()
   await Promise.all([
-    worker.add(1, 2).then(console.log),
-    worker.getUser(1).then(console.log),
+    worker.loadUser().then(console.log),
+    worker.calculatePi().then(console.log),
   ])
   await worker.exit()
 }
