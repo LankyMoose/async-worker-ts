69a642b224409e6863cf708134825dd0a84f13f3
Robby6Strings <rausten93@gmail.com>
2023-11-07 20:17:19 +1300

    refactor sandbox/browser-demo to use worker.clone()

--- sandbox/browser-demo/src/main.ts
+++ sandbox/browser-demo/src/main.ts
@@ -15,36 +15,54 @@ const worker = useWorker({
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <button id="btn" type="button">New Task</button>
-    <button id="cancel-btn" type="button">Stop Worker</button>
     <h4 id="total-iterations">Total Iterations: 0</h4>
     <ul style="list-style-type:none;margin:0;padding:0" id="progress-bars"></ul>
 `
 const btn = document.getElementById("btn")!
 const progressBars = document.getElementById("progress-bars")!
 const totalIterationsEl = document.getElementById("total-iterations")!
-const cancelButton = document.getElementById("cancel-btn")!
 
 let totalIterations = 0
 const iterationsPerTask = 250_000_000
 
 btn.addEventListener("click", () => {
+  const w = worker.clone()
+  const startTime = Date.now()
+
+  const li = document.createElement("li")
+  const durationText = Object.assign(document.createElement("span"), {
+    innerHTML: "0ms",
+  })
   const progressBar = Object.assign(document.createElement("progress"), {
     value: 0,
     max: 1,
   })
+  const cancelButton = Object.assign(document.createElement("button"), {
+    type: "button",
+    innerHTML: "Cancel",
+    onclick: () => {
+      w.exit()
+      // focus the sibling li -> button
+      // @ts-ignore
+      li.nextElementSibling?.querySelector("button").focus()
 
-  const li = document.createElement("li")
-  li.appendChild(progressBar)
+      li.remove()
+    },
+  })
+
+  li.append(progressBar, cancelButton, durationText)
   progressBars.appendChild(li)
 
-  worker
-    .calculatePi(iterationsPerTask)
+  w.calculatePi(iterationsPerTask)
     .onProgress((n) => {
       progressBar.value = n
       totalIterations += iterationsPerTask / 100
       totalIterationsEl.innerHTML = `Total Iterations: ${totalIterations.toLocaleString()}`
+      durationText.innerHTML = Date.now() - startTime + "ms"
     })
-    .then(() => (progressBar.value = 1))
-
-  cancelButton.addEventListener("click", () => worker.exit())
+    .then(() => {
+      progressBar.value = 1
+      w.exit()
+      cancelButton.innerText = "Remove"
+    })
 })
