2ca81593225df85628782e11b93b354d556f2afe
Robby6Strings <rausten93@gmail.com>
2023-11-07 19:39:16 +1300

    tweaking browser demo

--- sandbox/browser-demo/src/main.ts
+++ sandbox/browser-demo/src/main.ts
@@ -15,12 +15,14 @@ const worker = useWorker({
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <button id="btn" type="button">New Task</button>
+    <button id="cancel-btn" type="button">Stop Worker</button>
     <h4 id="total-iterations">Total Iterations: 0</h4>
     <ul style="list-style-type:none;margin:0;padding:0" id="progress-bars"></ul>
 `
 const btn = document.getElementById("btn")!
 const progressBars = document.getElementById("progress-bars")!
 const totalIterationsEl = document.getElementById("total-iterations")!
+const cancelButton = document.getElementById("cancel-btn")!
 
 let totalIterations = 0
 const iterationsPerTask = 250_000_000
@@ -30,13 +32,19 @@ btn.addEventListener("click", () => {
     value: 0,
     max: 1,
   })
+
   const li = document.createElement("li")
   li.appendChild(progressBar)
   progressBars.appendChild(li)
 
-  worker.calculatePi(iterationsPerTask).onProgress((n) => {
-    progressBar.value = n
-    totalIterations += iterationsPerTask / 100
-    totalIterationsEl.innerHTML = `Total Iterations: ${totalIterations.toLocaleString()}`
-  })
+  worker
+    .calculatePi(iterationsPerTask)
+    .onProgress((n) => {
+      progressBar.value = n
+      totalIterations += iterationsPerTask / 100
+      totalIterationsEl.innerHTML = `Total Iterations: ${totalIterations.toLocaleString()}`
+    })
+    .then(() => (progressBar.value = 1))
+
+  cancelButton.addEventListener("click", () => worker.exit())
 })
