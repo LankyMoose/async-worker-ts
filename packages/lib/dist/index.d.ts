4da397d2d55ff269099ef3f949af8f6aec68dd49
Robby6Strings <rausten93@gmail.com>
2023-11-13 14:46:22 +1300

    rm lib/dist

--- packages/lib/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Task } from "./task.js";
-import { IProcMap, AsyncWorkerClient } from "./types.js";
-export default function <const T extends IProcMap>(procMap: T): AsyncWorkerClient<T>;
-export declare function task<const T extends readonly unknown[], U>(fn: (this: Task<any, any>, ...args: T) => U): Task<T, U>;
--- packages/lib/dist/task.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Task<const T extends readonly unknown[], U> {
-    private readonly fn;
-    constructor(fn: (this: Task<any, any>, ...args: T) => U);
-    emit(event: string, data?: any): Promise<unknown>;
-    static getTaskFn(task: Task<any, any>): (this: Task<any, any>, ...args: any) => any;
-}
