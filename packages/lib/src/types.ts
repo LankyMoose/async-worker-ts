c2bdd0870c60c3dbeccc899d06d78fcadc9363df
Robby6Strings <rausten93@gmail.com>
2023-11-13 14:48:53 +1300

    imrpove promise type inference

--- packages/lib/src/types.ts
+++ packages/lib/src/types.ts
@@ -1,6 +1,7 @@
 import { Task } from "./task"
 
 type Func = (...args: any[]) => any
+type InferredPromiseValue<T> = T extends Promise<infer U> ? U : T
 
 export interface IProcMap {
   [key: string]: Func | Task<readonly unknown[], any> | IProcMap
@@ -18,7 +19,10 @@ export type AsyncWorkerClient<T extends IProcMap> = {
 }
 
 export type ProcedurePromise<T> = Promise<T> & {
-  on: (event: string, callback: (data?: any) => void) => ProcedurePromise<T>
+  on: (
+    event: string,
+    callback: (data?: any) => unknown
+  ) => ProcedurePromise<InferredPromiseValue<T>>
 }
 
 export type WorkerMessage = {
@@ -36,7 +40,11 @@ export type WorkerMessage = {
 }
 
 type InferredClientProc<T> = T extends Task<infer Args, infer E>
-  ? (...args: Args) => E extends ProcedurePromise<any> ? E : ProcedurePromise<E>
+  ? (
+      ...args: Args
+    ) => E extends ProcedurePromise<any>
+      ? E
+      : ProcedurePromise<InferredPromiseValue<E>>
   : T extends Func
   ? (...args: Parameters<T>) => ProcedurePromise<ReturnType<T>>
   : T extends IProcMap
